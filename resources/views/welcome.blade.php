0f503bf4e8f93c5c3b3705ba7da6b702dd0fa17b
MFW-1 <am64fw5@gmail.com>
2026-03-11 19:52:42 +0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Laravel Landing Page</title>
+    <style>
+        body{
+            font-family: Arial;
+            text-align:center;
+            background:#f4f4f4;
+        }
+        .hero{
+            margin-top:100px;
+        }
+        h1{
+            font-size:50px;
+        }
+        button{
+            padding:12px 25px;
+            background:#000;
+            color:#fff;
+            border:none;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="hero">
+<h1>Welcome to My Laravel Website</h1>
+<p>Deployed on AWS EC2 using GitHub Actions</p>
+
+<button>Get Started</button>
+</div>
+
+</body>
+</html>
